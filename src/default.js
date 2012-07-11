ea33562e73c2efababb8a940e8592c778da667dc
andy c <t2ahc@sad-panda.local>
2012-07-11 08:19:20 -0500

    write canvas-related boilerplate

---
+++ src/default.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function() {
+    var cvs = document.getElementById('canvas');
+    var wgl = cvs.getContext('moz-webgl') || cvs.getContext('webkit-3d');
+
+    wgl.clearColor(0.0, 0.0, 0.0, 1.0);
+    wgl.enable(wgl.DEPTH_TEST);
+
+    var resized = function() {
+        cvs.width = window.innerWidth;
+        cvs.height = window.innerHeight;
+        wgl.viewport(0, 0, cvs.width, cvs.height);
+    }
+    resized();
+    window.onresize = resized;
+
+    return;
+}, false);
